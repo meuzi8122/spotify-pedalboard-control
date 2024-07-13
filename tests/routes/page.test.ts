6f0d8a2b463648d4024d0ae98dc8b6ab7d32a3a8
meuzi8122 <kagiakete1234@gmail.com>
2024-07-13 18:57:22 +0900

    writableのモック(動かない)

--- tests/routes/page.test.ts
+++ tests/routes/page.test.ts
@@ -1,10 +1,28 @@
 import { render } from "@testing-library/svelte";
 import IndexPage from "../../src/routes/+page.svelte";
 
-describe("+page.svelte", async () => {
-  test("", async () => {
+describe("トップページのテスト", async () => {
+  test("ペダル数が0の場合はペダルステッパーが1つも表示されない", async () => {
+    vi.mock("store/pedal", async () => {
+      const { writable } = await import("svelte/store");
+      return writable([]);
+    });
+
     const { queryByTestId } = render(IndexPage);
 
     expect(queryByTestId("pedal-step")).toBe(null);
   });
+
+  // test("ペダル数が1の場合はペダルステッパーが1つ表示される", async () => {
+  //   vi.mock("store/pedal", async () => {
+  //     const { writable } = await import("svelte/store");
+  //     return writable([
+  //       { id: "dadfb805-2ed1-483e-88e5-fe69da8513fb", name: "", kind: "distortion", parameters: { gain: 1 } },
+  //     ]);
+  //   });
+
+  //   const { getByTestId } = render(IndexPage);
+
+  //   expect(getByTestId("pedal-step"));
+  // });
 });
